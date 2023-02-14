1b509bfbc60a4f077099f64bfd15f87cf3716e90
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-14 12:09:26 +0200

    fix:build

--- server/grails-app/init/testtask/BootStrap.groovy
+++ server/grails-app/init/testtask/BootStrap.groovy
@@ -2,7 +2,8 @@ package testtask
 
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext ->        
+        new User(login: 'admin', password: 'admin').save()        
     }
     def destroy = {
     }
