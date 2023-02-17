8216207b41963d06d70ac1899998203298061b0f
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-17 15:48:04 +0200

    third part

---
+++ server/grails-app/domain/testtask/User.groovy
@@ -0,0 +1,14 @@
+package testtask
+
+import grails.rest.Resource
+
+@Resource(uri='/user', namespace='/api', formats=['xml', 'json'])
+class User {
+
+    String login
+    String password
+    static constraints = {
+        login blank: false, unique: true
+        password blank: false
+    }
+}
---
+++ server/src/test/groovy/testtask/UserSpec.groovy
@@ -0,0 +1,18 @@
+package testtask
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UserSpec extends Specification implements DomainUnitTest<User> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test"() {
+        expect:"fix me"
+            true == false
+    }
+}
